15da0036a830eefeb244af1f7632a2aaa3df792f
Tomás Juárez <tomasjuarez.exa@gmail.com>
2016-02-26 15:44:37 -0300

    Delete old files

--- Ingles/src/controller/events/ListArtifactsEventAcceptor.java
+++
@@ -1,8 +0,0 @@
-package controller.events;
-
-/**
- * Created by Gandalf on 21/2/2016.
- */
-public interface ListArtifactsEventAcceptor {
-    void accept(ListArtifactsEventVisitor visitor);
-}
--- Ingles/src/controller/events/ListArtifactsEventVisitor.java
+++
@@ -1,9 +0,0 @@
-package controller.events;
-
-/**
- * Created by Gandalf on 21/2/2016.
- */
-public interface ListArtifactsEventVisitor {
-    public void visit(ListDecks event);
-    public void visit(ListCards event);
-}
