bb53a8df35be603368cce3d45e02a60fb4f96b1a
Guille12 <guillermopacheco12@hotmail.com>
2016-03-09 13:56:10 -0300

    Estrategias medium y  very difficult

--- src/model/DifficultStrategy.java
+++ src/model/DifficultStrategy.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -9,6 +10,21 @@ public class DifficultStrategy implements Strategy {
 
     @Override
     public String getAttribute(Card c) {
-        return "";
+
+        System.out.println("DIFICIL");
+        String nameAttribMore = "";
+        Double valueMore = 0.0;
+        List<String> attributes = c.getAttributes();
+
+        for(String attrib: attributes){
+            if(c.getAttribute(attrib) > valueMore){
+                nameAttribMore = attrib;
+                valueMore = c.getAttribute(attrib) ;
+            }
+        }
+
+        return nameAttribMore;
+
+
     }
 }
--- src/model/MediumStrategy.java
+++ src/model/MediumStrategy.java
@@ -8,6 +8,44 @@ public class MediumStrategy implements Strategy {
 
     @Override
     public String getAttribute(Card c) {
-        return "";
+
+        System.out.println("MEDIO");
+
+        String nameAttribLess = "";
+        Double valueLess = 10000.0;
+        List<String> attributes = c.getAttributes();
+
+        for(String attrib: attributes){
+            if(c.getAttribute(attrib) < valueLess){
+                nameAttribLess = attrib;
+                valueLess = c.getAttribute(attrib) ;
+            }
+        }
+
+
+        String nameAttribMore = "";
+        Double valueMore = 0.0;
+        for(String attrib: attributes){
+            if(c.getAttribute(attrib) > valueMore){
+                nameAttribLess = attrib;
+                valueMore = c.getAttribute(attrib) ;
+            }
+        }
+
+        String attribute = "";
+        Double value = 1000000.0;
+        System.out.println("Grande: "+valueMore +  "  Chico: " + valueLess);
+        Double media = valueMore-valueLess;
+
+        for(String attrib: attributes){
+            if(Math.abs(media - c.getAttribute(attrib)) < value){
+                System.out.println("ATRIBUTO: "+attrib+"  VALOR MEDIA: "+ Math.abs(media - c.getAttribute(attrib)));
+                attribute = attrib;
+                value = c.getAttribute(attrib) ;
+            }
+        }
+
+        System.out.println("VALOR ATRIBUTO SELECCIONADO:"+c.getAttribute(attribute));
+        return attribute;
     }
 }
